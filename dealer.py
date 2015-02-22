edfc5b7a5315e3be4834e82039748f1f4b8e0596
Michael Heasell <michael.heasell@gmail.com>
2015-02-22 16:01:04 +0000

    Extract utility functions from dealer

--- dealer.py
+++ dealer.py
@@ -1,6 +1,6 @@
 from redis import StrictRedis
 
-from hearts.util import deal_hands
+from hearts.util import deal_hands, find_winning_index, sum_points
 from hearts.services.game import GameService, GameEventQueueService
 
 import json
@@ -66,59 +66,6 @@ def process_end_round_event(game_id, round_number):
         queue_svc.raise_end_game_event(game_id)
     else:
         queue_svc.raise_start_round_event(game_id, round_number + 1)
-
-
-def find_winning_index(cards):
-    parsed_cards = map(parse_card, cards)
-
-    lead_suit = parsed_cards[0][0]
-
-    winner_rank = 0
-    winner_index = -1
-
-    for (index, (suit, rank)) in enumerate(parsed_cards):
-        if suit != lead_suit:
-            continue
-        numeric_rank = get_numeric_rank(rank)
-        if numeric_rank > winner_rank:
-            winner_rank = numeric_rank
-            winner_index = index
-
-    return winner_index
-
-
-def get_numeric_rank(str_rank):
-    if str_rank == "j":
-        return 11
-    if str_rank == "q":
-        return 12
-    if str_rank == "k":
-        return 13
-    if str_rank == "1":
-        return 14
-
-    return int(str_rank)
-
-
-def sum_points(cards):
-    return sum(map(point_value, cards))
-
-
-def parse_card(card):
-    suit = card[0]
-    rank = card[1:]
-    return suit, rank
-
-
-def point_value(card):
-    if card == "sq":
-        return 13
-
-    suit, rank = parse_card(card)
-    if suit == "h":
-        return 1
-
-    return 0
 
 
 def process_event(event_type, *args):
--- hearts/util.py
+++ hearts/util.py
@@ -44,3 +44,56 @@ def redis_key(*args):
     args = map(lambda x: x.replace("\\", "\\\\"), args)
     args = map(lambda x: x.replace(":", "\\:"), args)
     return ":".join(args)
+
+
+def find_winning_index(cards):
+    parsed_cards = map(parse_card, cards)
+
+    lead_suit = parsed_cards[0][0]
+
+    winner_rank = 0
+    winner_index = -1
+
+    for (index, (suit, rank)) in enumerate(parsed_cards):
+        if suit != lead_suit:
+            continue
+        numeric_rank = get_numeric_rank(rank)
+        if numeric_rank > winner_rank:
+            winner_rank = numeric_rank
+            winner_index = index
+
+    return winner_index
+
+
+def get_numeric_rank(str_rank):
+    if str_rank == "j":
+        return 11
+    if str_rank == "q":
+        return 12
+    if str_rank == "k":
+        return 13
+    if str_rank == "1":
+        return 14
+
+    return int(str_rank)
+
+
+def sum_points(cards):
+    return sum(map(point_value, cards))
+
+
+def parse_card(card):
+    suit = card[0]
+    rank = card[1:]
+    return suit, rank
+
+
+def point_value(card):
+    if card == "sq":
+        return 13
+
+    suit, rank = parse_card(card)
+    if suit == "h":
+        return 1
+
+    return 0
