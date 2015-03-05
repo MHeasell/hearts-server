026d9414075ea50420ad7103ab26a7f8e2b959e5
Michael Heasell <michael.heasell@gmail.com>
2015-03-05 16:11:23 +0000

    Start implementing the hearts game model

---
+++ hearts/game_model.py
@@ -0,0 +1,100 @@
+"""
+This is the main model for the hearts game.
+All the game domain logic lives here.
+It only knows how to play hearts.
+"""
+
+
+class GameStateError(Exception):
+    pass
+
+
+class RoundNotInProgressError(GameStateError):
+    pass
+
+
+class IncorrectPassPlayerError(GameStateError):
+    pass
+
+
+class CardsNotInHandError(GameStateError):
+    pass
+
+
+class CardsAlreadyPassedError(GameStateError):
+    pass
+
+
+class PlayersYetToPassError(GameStateError):
+    pass
+
+
+class HeartsGame(object):
+
+    def __init__(self):
+        self.state = "init"
+        self.hands = [None, None, None, None]
+        self.passed_cards = [None, None, None, None]
+        self.current_player = None
+
+    def get_score(self, player_index):
+        return 0
+
+    def start_round(self, hands):
+        for i in range(4):
+            self.hands[i] = list(hands[i])
+
+        self.state = "round"
+
+    def get_hand(self, player_index):
+        if self.state == "init":
+            raise RoundNotInProgressError()
+
+        hand = self.hands[player_index]
+        if hand is None:
+            return None
+
+        return hand[:]  # make a copy
+
+    def pass_cards(self, player_index, cards):
+        if self.state == "init":
+            raise RoundNotInProgressError()
+
+        if self.passed_cards[player_index] is not None:
+            raise CardsAlreadyPassedError()
+
+        # copy cards to prevent shenanigans
+        cards_to_pass = list(cards)
+
+        for card in cards_to_pass:
+            if card not in self.hands[player_index]:
+                raise CardsNotInHandError()
+
+        self.passed_cards[player_index] = cards_to_pass
+
+    def finish_passing(self):
+        for cards in self.passed_cards:
+            if cards is None:
+                raise PlayersYetToPassError()
+
+        for i, cards in enumerate(self.passed_cards):
+            for card in cards:
+                self.hands[i].remove(card)
+                self.hands[(i + 1) % 4].append(card)
+
+        # find the starting player
+        for i, hand in enumerate(self.hands):
+            if "c2" in hand:
+                self.current_player = i
+                break
+
+        assert self.current_player is not None
+
+        # move to playing state
+        self.state = "round-playing"
+
+    def get_current_player(self):
+        if self.state == "init" or self.state == "round":
+            raise RoundNotInProgressError()
+
+        return self.current_player
